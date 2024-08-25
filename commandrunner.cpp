f932260e7c4f04fbc3c2d1966902507a4f0a732c
troy600 <bazartroy531@gmail.com>
2024-08-25 19:44:47 +0800

    nan

--- commandrunner.cpp
+++ commandrunner.cpp
@@ -7,10 +7,10 @@ int main(void) {
   int seggs = 9;
   string ccsict;
   std::cout << "enter command to run \n >>";
-  std::cin >> ccsict;
+  getline(cin, ccsict);
 
-  string newcccsict = "'" + ccsict + "'";
-
+  string newcccsict = ccsict;
+  cout << ccsict << "\n";
   const char *newerccsict = newcccsict.c_str();
   system(newerccsict);
 
---
+++ input output getline with class.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+class bsdsa {
+public:
+    std::string dsas, ccsist;
+    bsdsa(const std::string name, const std::string lastname) {
+        dsas = name;
+        ccsist = lastname;
+    }
+    void display() {
+        std::cout << "welcome. Firstname: " << dsas << " lastname: " << ccsist;
+        //extract the info;
+        std::string studentinfo = "echo 'firstname: " + dsas + " lastname: " + ccsist + "' >> studentinfo.txt";
+        const char *data = studentinfo.c_str();
+        system(data);
+    }
+};
+
+int main(void){
+    std::pmr::string Pangalan, apilyedo;
+
+    std::cout << "hello type your Firstname \n >> ";
+
+    std::getline(std::cin, Pangalan);
+
+    std::cout << "type your lastname\n >> ";
+
+    std::getline(std::cin, apilyedo);
+
+    //fix the shit
+    const char *first = Pangalan.c_str();
+    const char *last = apilyedo.c_str();
+
+    bsdsa object(first, last);
+
+    object.display();
+    return 200;
+}
