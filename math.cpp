21df6b98661b60a8e72724233007bba07fd5061e
troy600 <bazartroy531@gmail.com>
2024-09-04 16:07:43 +0800

    set examples added

---
+++ math.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class inf {
+public:
+    std::string ufirst, ulast, umiddle, uage;
+    inf(string first, string last, string middle, string age){
+        ufirst = first;
+        ulast = last;
+        umiddle = middle;
+        uage = age;
+    }
+    void parse() {
+        ofstream file("studentinfo.txt");
+        if (file.is_open()) {
+            file << "Name: " << ufirst << "Lastname" << ulast << "MiddleName: " << umiddle << "age: " << uage;
+            file.close();
+        }else {
+            cout << "unable to open file";
+            }
+        
+        cout << "\n hello " << ufirst; 
+
+
+    }
+};
+
+int main(void) {
+    return 0;
+}
---
+++ set.py
@@ -0,0 +1,8 @@
+#this is set
+B = 'banana', 'tree', 'txt', 'things', 'my things'
+
+B += 'a', 'b', 'c', 'd', 'banana', 'txt'
+
+C = {*B}
+#concept of set idk(no same name)
+print(C)
