f5ec114e6e8fc5686d5201831fee7cf19e5f24e4
troy600 <bazartroy531@gmail.com>
2024-10-02 22:45:44 +0800

    c lang

---
+++ practice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[99];
+    printf("hello who are you? \n >> ");
+    scanf("%s" ,&name);
+    
+    printf("hello %s how's your day hmm?", name);
+
+    return 69;
+}
