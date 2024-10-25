f93402c25e53ed8f4b33b3432d7113e80dfcb21f
troy600 <bazartroy531@gmail.com>
2024-10-25 10:31:41 +0800

    display timedate on java

---
+++ timetade.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class hello {
+    public static void main(String[] args) {
+        Scanner this = new Scanner(System.in);
+        System.out.println("hi dsa");
+    }
+}
